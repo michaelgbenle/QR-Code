3ffb5da9afcf16a9d12962f752a0a22c2dea7f72
michaelgbenle <michaelgbenle@gmail.com>
2022-07-24 14:44:31 -0700

    working

--- main.go
+++ main.go
@@ -12,4 +12,5 @@ type Page struct {
 func HomePage(w http.ResponseWriter, r *http.Request) {
 		p:= Page{Title: "Generate QR Code"}
 		t,_:= template.ParseFiles("generator.html")
+		t.Execute(w,p)
 }
